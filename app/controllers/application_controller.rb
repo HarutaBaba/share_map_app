a296d89add17b07ecf0bb3e8c6da063b6282bc56
Haruta Baba <bby0622@gmail.com>
2019-12-05 03:30:18 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
   def home
   end
   
+  class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  end
+  
 end
     
