3c74f04256d001b0ead6f1a67c6790c24bdfdcc9
Haruta Baba <bby0622@gmail.com>
2019-12-09 02:19:23 +0000

    login-test

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   root to: 'maps#index'
   resources :maps, only: [:index]
   
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
 
